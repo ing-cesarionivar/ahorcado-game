12e612c2f64d72f6f178d0d2ff23ae1352af4195
Cesario Nivar <ing.cesarionivar@gmail.com>
2020-03-29 12:16:01 -0400

    Verificar si el usuario gana o pierde

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,68 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'ahorcado';
+
+  palabra = 'AGUACATE';
+  palabraOculta = '';
+
+  intentos = 0;
+
+  gano = false;
+  perdio = false;
+
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+            'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  constructor() {
+
+    this.palabraOculta = '_ '.repeat(this.palabra.length);
+    
+  }
+
+  comprobar( letra ) {
+
+    this.existeLetra(letra);
+    
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+
+    for (let i = 0; i < this.palabra.length; i++) {
+
+      if (this.palabra[i] === letra) {
+        palabraOcultaArr[i] = letra;
+      }
+    }
+
+    this.palabraOculta = palabraOcultaArr.join(' ');
+    this.verificaGane();
+
+  }
+
+  verificaGane() {
+
+    const palabraArr = this.palabraOculta.split(' ');
+    const palabraEvaluar = palabraArr.join('');
+
+    console.log(palabraEvaluar);
+    
+    
+    
+
+  }
+
+  existeLetra( letra ) {
+
+    if (this.palabra.indexOf(letra) >= 0 ){
+      // console.log('Letra existe: ' + letra);
+      
+    } else {
+      // console.log('Letra NO existe: ' + letra);
+      this.intentos++;
+    }
+
+  }
+
 }
